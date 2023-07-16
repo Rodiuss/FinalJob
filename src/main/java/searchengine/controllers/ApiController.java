d54a7e64cc0b732810b6d988e9494ad146713588
Rodiuss <rodion.boy@mail.ru>
2023-07-16 15:17:47 +0300

    Done indexPage part

--- src/main/java/searchengine/controllers/ApiController.java
+++ src/main/java/searchengine/controllers/ApiController.java
@@ -2,9 +2,7 @@ package searchengine.controllers;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import searchengine.dto.statistics.BadRequest;
 import searchengine.dto.statistics.Response;
 import searchengine.dto.statistics.StatisticsResponse;
@@ -46,6 +44,20 @@ public class ApiController {
         } else {
             return new ResponseEntity<>(new BadRequest(false,
                     "Indexing was not stopped because it was not started"), HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    @PostMapping("/indexPage")
+    public ResponseEntity<Object> indexPage(@RequestParam(name = "url") String url) {
+        if (url.isEmpty()) {
+            return new ResponseEntity<>(new BadRequest(false, "Page not specified"), HttpStatus.BAD_REQUEST);
+        } else {
+            if (indexingService.urlIndexing(url) == true) {
+                return new ResponseEntity<>(new Response(true), HttpStatus.OK);
+            } else {
+                return new ResponseEntity<>(new BadRequest(false, "Required page out of configuration file"),
+                        HttpStatus.BAD_REQUEST);
+            }
         }
     }
 }
