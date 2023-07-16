62281b2127701d14271a9e5f0d085e872dc648bf
Rodiuss <rodion.boy@mail.ru>
2023-07-16 14:57:49 +0300

    Done index part

--- src/main/java/searchengine/controllers/ApiController.java
+++ src/main/java/searchengine/controllers/ApiController.java
@@ -1,10 +1,14 @@
 package searchengine.controllers;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import searchengine.dto.statistics.BadRequest;
+import searchengine.dto.statistics.Response;
 import searchengine.dto.statistics.StatisticsResponse;
+import searchengine.services.IndexingService;
 import searchengine.services.StatisticsService;
 
 @RestController
@@ -12,13 +16,36 @@ import searchengine.services.StatisticsService;
 public class ApiController {
 
     private final StatisticsService statisticsService;
+    private final IndexingService indexingService;
 
-    public ApiController(StatisticsService statisticsService) {
+    public ApiController(StatisticsService statisticsService,
+                         IndexingService indexingService) {
         this.statisticsService = statisticsService;
+        this.indexingService = indexingService;
     }
 
     @GetMapping("/statistics")
     public ResponseEntity<StatisticsResponse> statistics() {
         return ResponseEntity.ok(statisticsService.getStatistics());
+    }
+
+    @GetMapping("/startIndexing")
+    public ResponseEntity<Object> startIndexing() {
+        if (indexingService.indexingAll()) {
+            return new ResponseEntity<>(new Response(true), HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(new BadRequest(false, "Indexing not started"),
+                    HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    @GetMapping("/stopIndexing")
+    public ResponseEntity<Object> stopIndexing() {
+        if (indexingService.stopIndexing()) {
+            return new ResponseEntity<>(new Response(true), HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(new BadRequest(false,
+                    "Indexing was not stopped because it was not started"), HttpStatus.BAD_REQUEST);
+        }
     }
 }
